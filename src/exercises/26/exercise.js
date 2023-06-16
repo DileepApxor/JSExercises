6fdbe2b9b6087c3ccdb8229bae85f517bc35104c
DileepApxor <dileepchandra.yanamadala@apxor.com>
2023-06-16 18:21:51 +0530

    Exercise 26-30 done

--- src/exercises/26/exercise.js
+++ src/exercises/26/exercise.js
@@ -1,4 +1,4 @@
-// Create a Counter using closures.
+//   Create a Counter using closures.
 // - counter should have "increment","decrement" and "value" functions.
 // - Is there a anonymus function used here? Is so where is it?
 // - What is an IIFE? Have we used one here?
@@ -7,4 +7,31 @@
 
 const counter = (function () {
   //Your code goes here.
+  let count = 0;  // the count variable here acts as a private variable, it cannot be accessed outside this IIEF function
+  function increment() {
+    count++;
+  }
+
+  function decrement() {
+    count--;
+  }
+
+  function value() {
+    return count;
+  }
+
+  return {increment,decrement,value}  //here we've returned the object containing increment,decrement and value function
+  
 })();
+
+//IIFE means immediately invoked function expression is used to create the function with private variables and functions
+//we cant access the varaibles which are declared inside the IIFE outisde the scope of the function
+
+//here the anonymous function is used which is nothing but ({....})() this is the IIFE function which can be executed as soon as defined
+
+counter.increment();
+console.log(counter.value());
+counter.decrement();
+counter.decrement();
+counter.decrement();
+console.log(counter.value());
--- src/exercises/27/exercise.js
+++ src/exercises/27/exercise.js
@@ -1,17 +1,20 @@
-// What is a callback?
+// What is a callback?  
 
-function welcome(name) {
-  setTimeout(() => {
-    console.log(`Welcome ${name}`);
+// callback function is a function that is passed as an argument to another function and is executed inside that function. 
+// The function receiving the callback can then invoke the callback at any given time or condition.
+
+function welcome(name,callback) {
+  setTimeout(() => {                   //this function will executed after one second
+    console.log(`Welcome ${name}`);    //it will print the name of the person which is passing as an argument 
+    callback();                        //it is a callback function it will be executed after the printing of the above content in the terminal
   }, 1000);
 }
 
 function success() {
-  console.log("Greetings successful!!");
+  console.log("Greetings successful!!");  //its a success function which prints Greetings successfull 
 }
 
-welcome();
-success();
+welcome("Dileep",success);     // here we called welcome function with passing an argument name and calling the callback function success
 
 // In the above example:
 // - Before welcoming, success message is printed.
