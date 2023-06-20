751e952cb2412778a600403a96ab2f7b748793a9
DileepApxor <dileepchandra.yanamadala@apxor.com>
2023-06-20 18:37:59 +0530

    JsExercise 21-25 done

--- src/exercises/21/exercise.js
+++ src/exercises/21/exercise.js
@@ -1,1 +1,12 @@
 // What are the different levels of logs and how do we print them to the console?
+console.log("This is a log message");     
+// used to print the general messsage in the terminal
+
+console.info("This is an info message");
+// it is used to show the information to the user who is using the app/webpage
+
+console.warn("This is a warning message");
+// it shows the warning message like when there is a bad behavioural in the process or which leads to an error
+
+console.error("This is an error message");
+// it shows an error message in the console like a critical error showed in the red coloured text
--- src/exercises/22/exercise.js
+++ src/exercises/22/exercise.js
@@ -3,11 +3,14 @@
 // - What is "Watch" in chrome debugging console. Share a screenshot of the watch variables at each step.
 
 function randomCalculator() {
-  let x = 10,
-    y = 0;
-  x = x * 10 + 1 - (2 / 2) * 3;
-  y = x - 1 + 3 * 10 - 2;
-  x = x * 2 - (1 / 3) * 3;
-  x = x + 10 - 2 - 3 * 2;
+  debugger;  //when the debugger attached to this code we can access through every breakpoint from start to end of the code execution
+             // i've attached some screenshot pngs when im running the code going through every breakpoint
+  let x = 10, y = 0;
+  x = x * 10 + 1 - (2 / 2) * 3; //98
+  y = x - 1 + 3 * 10 - 2;       //125
+  x = x * 2 - (1 / 3) * 3;      //195
+  x = x + 10 - 2 - 3 * 2;       //197
   console.log(x);
 }
+
+randomCalculator()
