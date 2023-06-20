44941ae2abf717fa3118a46faa51fe300f96a9dd
DileepApxor <dileepchandra.yanamadala@apxor.com>
2023-06-20 18:37:07 +0530

    JsExercise 11-15 done

--- src/exercises/15/exercise.js
+++ src/exercises/15/exercise.js
@@ -1,13 +1,21 @@
 // Convert the below functions into arrow functions.
 
-const greetings = function () {
+/*const greetings = function () {
   return "Hello World";
-};
+};*/
 
-const isEven = function (num) {
+const greetings = () => "Hello World"; 
+
+/*const isEven = function (num) {
   if (num % 2 == 0) {
     return true;
   } else {
     return false;
   }
-};
+};*/
+
+const isEven = num => num%2==0;   //this arrow function returns true if num is divisible by 2 or it returns false
+
+console.log(greetings())
+console.log(isEven(6))
+console.log(isEven(9))
